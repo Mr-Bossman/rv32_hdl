68dbf0d43f0e629038a8d690df22468307a0d0b6
Jesse Taube <Mr.Bossman075@gmail.com>
2023-03-02 09:26:35 -0500

    waves

--- Makefile
+++ Makefile
@@ -16,7 +16,7 @@ CROSS_COMPILE=riscv32-linux-
 all: ${PREFIX_NAME}
 
 ${PREFIX_NAME} : ${V_SOURCES} ${CPP_SOURCES}
-	verilator -Wall --Mdir ${BUILD_DIR} -cc ${V_SOURCES} -prefix ${PREFIX_NAME} -CFLAGS "${CPPFLAGS}" ${CPP_SOURCES} --exe --top-module soc_top
+	verilator --trace-fst -Wall --Mdir ${BUILD_DIR} -cc ${V_SOURCES} -prefix ${PREFIX_NAME} -CFLAGS "${CPPFLAGS}" ${CPP_SOURCES} --exe --top-module soc_top
 	$(MAKE) -C ${BUILD_DIR} -f ${PREFIX_NAME}.mk ${PREFIX_NAME}
 	cp ${BUILD_DIR}/${PREFIX_NAME} .
 
@@ -33,7 +33,7 @@ run_tests: all tests
 rv32asm: src/rv32.s src/rv32.lds
 	${CROSS_COMPILE}as -mlittle-endian src/rv32.s -o ${BUILD_DIR}/example.o
 	${CROSS_COMPILE}ld -melf32lriscv --gc-sections -T src/rv32.lds ${BUILD_DIR}/example.o -o ${BUILD_DIR}/example.elf
-	${CROSS_COMPILE}objcopy -O verilog --verilog-data-width=4 -j .text ${BUILD_DIR}/example.elf test.vh
+	${CROSS_COMPILE}objcopy -O verilog --verilog-data-width=4 --reverse-bytes=4  -j .text ${BUILD_DIR}/example.elf test.vh
 	sed -i s/@.*//g test.vh
 
 clean_exe:
--- src/test.cpp
+++ src/test.cpp
@@ -1,4 +1,5 @@
 #include "verilated.h"
+#include "verilated_fst_c.h"
 #define CONCAT(A, B) A##B
 #define CAT(A, B) CONCAT(A, B)
 #define SUFF(x) CONCAT(x,_)
@@ -11,20 +12,25 @@
 static uint64_t instruction_count = 0;
 static uint64_t clock_count = 0;
 static VPREFIX* sim = NULL;
+static VerilatedContext* contextp =  NULL;
+static VerilatedFstC* tfp = NULL;
 
 static void exit_now(int signo){
 	puts("\n\n");
 	for(int i = 0; i < 32;i++){
-		printf("regs %d: 0x%0x\n",i,sim->rootp->soc_top__DOT__riscv_cpu__DOT__regfile[i]);
+	//	printf("regs %d: 0x%0x\n",i,sim->rootp->soc_top__DOT__riscv_cpu__DOT__regfile[i]);
 	}
-	for(int i = 0; i < 32;i++){
-		printf("mem 0x%02x: 0x%08lx\n",i*4,sim->rootp->soc_top__DOT__ram__DOT__mem[i]);
-	}
+	//for(int i = 0; i < 32;i++){
+	//	printf("mem 0x%02x: 0x%08lx\n",i*4,sim->rootp->soc_top__DOT__ram__DOT__mem[i]);
+//	}
 	for(int i = 0; i < 4;i++){
-		printf("sys_mem 0x%02x: 0x%08lx\n",i*4,sim->rootp->soc_top__DOT__system__DOT__mem[i]);
+		//printf("sys_mem 0x%02x: 0x%08lx\n",i*4,sim->rootp->soc_top__DOT__system__DOT__mem[i]);
 	}
+	tfp->close();
 	sim->final();
+	delete tfp;
 	delete sim;
+	delete contextp;
 	printf("IPC: %lf instructions/clock\nCPI: %lf clock/instructions\n",(double)instruction_count/clock_count,(double)clock_count/instruction_count);
 	exit(signo);
 }
@@ -32,20 +38,32 @@ static void exit_now(int signo){
 int main(int argc, char **argv, char **env) {
 	signal(SIGINT, exit_now);
 	Verilated::commandArgs(argc, argv);
+	contextp = new VerilatedContext;
+	tfp = new VerilatedFstC;
 	sim = new VPREFIX;
+	Verilated::traceEverOn(true);
+  sim->trace(tfp, 99);  // Trace 99 levels of hierarchy (or see below)
 	sim->rts = 0;
+	contextp->timeInc(1);
 	sim->eval();
+  tfp->dump(contextp->time());
 	sim->rts = 1;
+	contextp->timeInc(1);
 	sim->eval();
+  tfp->dump(contextp->time());
 	sim->rts = 0;
 	uint32_t oldpc = sim->oldpc;
 	while(!sim->halted){
 		//printf("microop_pc 0x%0x pc: 0x%0x\n",sim->odat,sim->oldpc);
 		//usleep(50000);
 		sim->clk = 0;
+		contextp->timeInc(1);
+  	tfp->dump(contextp->time());
 		sim->eval();
 		sim->clk = 1;
+		contextp->timeInc(1);
 		sim->eval();
+  	tfp->dump(contextp->time());
 		if(sim->oldpc != oldpc){
 			oldpc = sim->oldpc;
 			instruction_count++;
